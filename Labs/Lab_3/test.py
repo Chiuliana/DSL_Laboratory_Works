015027fb63263ddcf0c88618e9009c255896f776
Chiuliana <chichioiuliana@gmail.com>
2024-03-17 16:36:13 +0200

    define Position Class

--- Labs/Lab_3/test.py
+++ Labs/Lab_3/test.py
@@ -19,3 +19,25 @@ class Error:
 class IllegalCharError(Error):
     def __init__(self, pos_start, pos_end, details):
         super().__init__(pos_start, pos_end, 'Illegal Character', details)
+
+# POSITION
+class Position:
+    def __init__(self, idx, ln, col, fn, ftxt):
+        self.idx = idx
+        self.ln = ln  # Line number
+        self.col = col
+        self.fn = fn  # File name
+        self.ftxt = ftxt  # Full text of the file
+
+    def advance(self, current_char):
+        self.idx += 1  # Moving to next character
+        self.col += 1  # Moving to next column
+
+        if current_char == '\n':
+            self.ln += 1  # Moving to next line
+            self.col = 0  # Reset column number
+
+        return self
+
+    def copy(self):
+        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)  # Creating a copy of the current position
